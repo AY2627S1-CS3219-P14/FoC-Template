6fb704ff64a67bda15b2be8cc390f489c767e473
meloppeitreet <65459059+meloppeitreet@users.noreply.github.com>
2026-09-22 19:58:57 +0800

    Add Testcontainers support for UserApplicationTests

--- user-service/src/test/java/com/campuscouriers/user/UserApplicationTests.java
+++ user-service/src/test/java/com/campuscouriers/user/UserApplicationTests.java
@@ -1,11 +1,15 @@
 package com.campuscouriers.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+@SpringBootTest
+@Testcontainers
 class UserApplicationTests {
 
 	@Container
